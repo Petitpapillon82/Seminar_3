2a1c63c75e4a1637d0dfb05f2221a9355eb11ab1
Irina <petitpapillon82@gmail.com>
2022-08-25 23:19:33 +0400

    Add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x==1)
+Console.WriteLine($"Диапазон значений в {x} четверти: (x;y)");
+else if (x==2)
+Console.WriteLine($"Диапазон значений в {x} четверти: (x;-y)");
+else if (x==3)
+Console.WriteLine($"Диапазон значений в {x} четверти: (-x;-y)");
+else if (x==4)
+Console.WriteLine($"Диапазон значений в {x} четверти: (-x;y)");
+else
+Console.WriteLine($"Ошибка ввода, введите цифру от 1 до 4");
